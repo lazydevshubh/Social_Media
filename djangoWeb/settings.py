57478e53b461334667ebd72e61e7e337d8cd173a
Shubham Gupta <lazydevshubh@gmail.com>
2020-06-23 23:39:31 +0530

    static files added

--- djangoWeb/settings.py
+++ djangoWeb/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_REDIRECT_URL="blog-home"
 LOGIN_URL="login"
